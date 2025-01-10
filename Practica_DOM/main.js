9105554682f5ebd53abac3676af5c47661c58ffa
Serrano1997 <vicentserrano1997@gmail.com>
2025-01-10 16:41:59 +0100

    Commit: Crear array de productos

---
+++ Practica_DOM/main.js
@@ -0,0 +1,51 @@
+
+const products = [
+  {
+    name: 'Martillo de Acero',
+    price: 25,
+    image: './Assets/Productos/martillohierro.webp',
+    link: 'https://totherramienta.com/products/martillo-profesional-con-mango-de-fibra-2kg?_pos=3&_sid=78c1d8cab&_ss=r'
+  },
+  {
+    name: 'Destornillador',
+    price: 10,
+    image: './Assets/Productos/desantornillador.webp',
+    link: 'https://totherramienta.com/products/juego-14-destornilladores-plano-estrella?_pos=2&_sid=1545f4d39&_ss=r'
+  },
+  {
+    name: 'Juego de Llaves',
+    price: 30,
+    image: './Assets/Productos/juegodellaves.webp',
+    link: 'https://totherramienta.com/products/juego-llaves-combinadas-xxl-bgs-34-50mm?_pos=12&_sid=5734ce39a&_ss=r'
+  },
+  {
+    name: 'Sierra Circular',
+    price: 18,
+    image: './Assets/Productos/sierracircular.webp',
+    link: 'https://totherramienta.com/products/sierra-electrica-circular-brushless?_pos=3&_sid=273c0b59f&_ss=r'
+  },
+  {
+    name: 'Taladro Perforador',
+    price: 50,
+    image: './Assets/Productos/taladroperforador.webp',
+    link: 'https://totherramienta.com/products/taladro-percutor-2-velocidades-1?_pos=2&_sid=044306fde&_ss=r'
+  },
+  {
+    name: 'Alicates Terminales',
+    price: 15,
+    image: './Assets/Productos/Alicatesterminales.webp',
+    link: 'https://totherramienta.com/products/alicates-terminales-no-aislados-8-3-4?_pos=1&_sid=db45c8d99&_ss=r'
+  },
+  {
+    name: 'Cinta Métrica',
+    price: 8,
+    image: './Assets/Productos/cintametrica.webp',
+    link: 'https://totherramienta.com/products/jbm-53026-cinta-metrica-5m-x-25mm?_pos=2&_sid=54bc17e4b&_ss=r'
+  },
+  {
+    name: 'Lijadora Eléctrica',
+    price: 40,
+    image: './Assets/Productos/lijadoraelectrica.avif',
+    link: 'https://totherramienta.com/products/lijadora-rotativa-20v?_pos=3&_sid=71ac4b35a&_ss=r'
+  }
+];
